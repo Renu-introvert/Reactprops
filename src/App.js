be0ed9a66544e4aad8de1302767fbec4ffdb6767
Renu-introvert <renugongada4@gmail.com>
2022-11-30 16:52:33 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+ function App(){
+  return(
+    <section>
+          <PropsExample name="Renu" age="20"/>
+          <PropsExample name="manasa" age="20"/>
+          { /*<h1>feelings</h1>
+          <p>some days are bad</p>
+          <img src="https://wallpaperaccess.com/full/507560.jpg" width="25%" alt="first"/>
+          <ClassComponent/>*/}
+     </section>     
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
